83c0d3f9b2c9b18e4f4d6e7e1518c4adbe576029
unknown <Admin@A19B50025.educacion.local>
2021-04-09 21:20:04 -0400

    aun no terminado

---
+++ interacciones.js
@@ -0,0 +1,7 @@
+// -----------------SCROOLL REVEAL--------
+
+ScrollReveal().reveal('.contenedor');
+ScrollReveal().reveal('.inicio', {delay:500});
+// ScrollReveal().reveal('.quien-soy', {delay:500});
+ScrollReveal().reveal('.social', {delay:500});
+ScrollReveal().reveal('.footer-links', {delay:500});
