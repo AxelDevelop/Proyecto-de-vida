bf4ecec457221c760c886ed0e240eaa7d08b70e3
unknown <Admin@A19B50025.educacion.local>
2021-04-10 14:30:09 -0400

    cambio

--- interacciones.js
+++ interacciones.js
@@ -1,7 +1,1 @@
-// -----------------SCROOLL REVEAL--------
 
-ScrollReveal().reveal('.contenedor');
-ScrollReveal().reveal('.inicio', {delay:500});
-// ScrollReveal().reveal('.quien-soy', {delay:500});
-ScrollReveal().reveal('.social', {delay:500});
-ScrollReveal().reveal('.footer-links', {delay:500});
